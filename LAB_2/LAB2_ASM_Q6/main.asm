e4eabca5baae44d11bb358edbd128d5361afb568
jerbo2 <99144447+jerbo2@users.noreply.github.com>
2022-11-08 21:24:02 -0500

    Add files via upload

---
+++ LAB_2/LAB2_ASM_Q6/main.asm
@@ -0,0 +1,55 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+			mov #2D97h, R4;
+			mov #6239h, R5;
+			mov #3427h, R6; Third Number (Wasn't Specified)
+
+					  ; For and ops
+			mov R5, R8; (to not change numbers)
+
+			call #and_operation;
+			jmp $;
+
+and_operation:
+			and R4, R8;
+			and R6, R8;
+			mov R8, &203Ch
+			ret;
+
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
